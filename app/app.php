2e3a160299e526c4d2728ece8db0750326f219fd
Alex Costa & Steve Smietana <student@epicodus.com>
2015-08-04 09:33:57 -0700

    Made initial files and stuff

---
+++ app/app.php
@@ -0,0 +1,12 @@
+<?php
+  require_once __DIR__."/../vendor/autoload.php";
+
+  $app = new Silex\Application();
+
+  $app->get("/hello", function() {
+    return "Hello friend!";
+  });
+
+  return $app;
+
+?>
